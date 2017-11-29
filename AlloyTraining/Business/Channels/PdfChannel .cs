4668014c790d59b52ad4b7366c99278c657effca
Svitlana Paradovska <svitlana.paradovska@hotmail.com>
2017-11-29 16:06:23 +0100

    initial

---
+++ AlloyTraining/Business/Channels/PdfChannel .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Web;
+
+namespace AlloyTraining.Business.Channels
+{
+    public class PdfChannel :DisplayChannel
+    {
+        public override bool IsActive(HttpContextBase context)
+        {
+            return context.Request.QueryString["pdf"] == 1.ToString();
+        }
+
+        public override string ChannelName
+        {
+            get { return "PDF"; }
+        }
+    }
+}
